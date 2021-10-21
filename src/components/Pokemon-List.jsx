8d1740e98cb31bdc583592610647ba658c4e9426
GregoriusRicky <44605991+sknk1@users.noreply.github.com>
2021-10-22 00:02:17 +0700

    Update Pokemon

--- src/components/Pokemon-List.jsx
+++ src/components/Pokemon-List.jsx
@@ -4,24 +4,39 @@ import CardPokemon from "./CardPokemon";
 
 function PokemonList() {
 
-    const [offset, setOffset] = useState("");
+    const [offset, setOffset] = useState(0);
     const [pokemonDetail, setPokemonDetail] = useState([]);
 
-    async function getAllPokemon() {
-        const url = "https://guarded-chamber-41398.herokuapp.com/getAllPokemons/0";
-        await axios.get(url)
-        .then(resp => {
-            const resData = resp.data.results;
-            // console.log(resData);
-            resData.forEach( async pokemon => {
-                const pokemunUrl = pokemon.url;
-                await axios.get(pokemunUrl)
-                .then(async resp => {
-                    setPokemonDetail(currentList => [...currentList, resp.data]);
-                    await pokemonDetail.sort((a, b) => a.id - b.id)
-                });
-            });
-        });  
+    async function getAllPokemon(noOffset) {
+        setOffset(noOffset + 20);
+        const url = "https://guarded-chamber-41398.herokuapp.com/getAllPokemons/" + offset;
+        const allPokemon = await axios.get(url);
+        await Promise.all(allPokemon.data.results.map(async (item) => {
+            const pokemonDetail2 = await axios.get(item.url);
+            pokemonDetail.sort((a, b) => a.id - b.id);
+            setPokemonDetail(currentList => [...currentList, pokemonDetail2.data]);
+            setPokemonDetail(currentList => currentList.sort((a, b) => a.id - b.id));
+            return item + 1;
+        }));
+
+        // .then(resp => {
+        //     const resData = resp.data.results;
+
+        //     await Promise.all(resData.map(async (item) => {
+        //         const pokemonDetail = await axios.get(pokemunUrl);
+        //         setPokemonDetail(currentList => [...currentList, pokemonDetail.data]);
+        //         return item + 1;
+        //     }));
+
+        //     // resData.forEach( async pokemon => {
+        //     //     const pokemunUrl = pokemon.url;
+        //     //     await axios.get(pokemunUrl)
+        //     //     .then(async resp => {
+        //     //         setPokemonDetail(currentList => [...currentList, resp.data]);
+        //     //         await pokemonDetail.sort((a, b) => a.id - b.id)
+        //     //     });
+        //     // });
+        // });  
         // const data = await response.data;
         // setPokemonData(response.data);
 
@@ -32,9 +47,13 @@ function PokemonList() {
         //     await pokemonDetail.sort((a, b) => a.id - b.id)
         // });
     }
+
+    async function updateNum(){
+        await getAllPokemon();
+    }
     
     useEffect(() => {
-        getAllPokemon();
+        getAllPokemon(0);
     }, [])
 
     return(
@@ -51,6 +70,11 @@ function PokemonList() {
                                 types = {pokemon.types}
                             />
                         )}
+                    </div>
+                    <div className="d-flex justify-content-center" onClick={updateNum}>
+                        <a className="btn btn-custom">
+                            LOAD MORE
+                        </a>
                     </div>
                 </div>
             </div>
